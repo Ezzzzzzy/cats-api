0aaaaedf7f9078de181b52e13b11659d2543d837
Ezra Paz <jrpaz@bayad.com>
2022-05-05 14:46:47 +0800

    feat: add search to get all cats api

--- src/controllers/catsController.js
+++ src/controllers/catsController.js
@@ -2,19 +2,20 @@ import Cat from '../models/catsModel.js'
 import paginate from '../helper/paginate.js';
 
 export const show = async (req, res) => {
-    let { page, pageSize, sort } = req.query
+    let { page, pageSize, sort, search } = req.query
 
-    let query = {}
-    if (sort != null) {
-        sort = sort.split(',')
-        query.order = [sort]
+    let query = {
+        where: search ? { where: { name: search } } : {},
+        order: sort ? { order: [sort.split(',')] } : {}
     }
-
 
     try {
         let cat = await Cat.findAndCountAll(
             paginate(
-                query,
+                {
+                    ...query.where,
+                    ...query.order
+                },
                 { page, pageSize }
             )
         );
